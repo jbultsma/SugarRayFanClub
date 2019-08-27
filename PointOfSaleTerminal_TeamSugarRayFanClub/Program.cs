05ff424acf3fd7db5367074459f93c527928d065
maricela <laram@mail.gvsu.edu>
2019-08-26 20:12:37 -0400

    Commit 5

---
+++ PointOfSaleTerminal_TeamSugarRayFanClub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PointOfSaleTerminal_TeamSugarRayFanClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Team Sugar Ray Fanclub!");
+            Console.WriteLine("This is a test commit");
+        }
+    }
+}
