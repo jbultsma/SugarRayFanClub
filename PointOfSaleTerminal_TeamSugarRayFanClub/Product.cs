3b3d425bb68cc7adee2400b6f170f52e0ed66e5b
Sam Thomson <samuelthomson62@gmail.com>
2019-08-28 19:59:32 -0400

    Here we gogit add .

---
+++ PointOfSaleTerminal_TeamSugarRayFanClub/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOfSaleTerminal_TeamSugarRayFanClub
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public string Catagory { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, string catagory, string description, double price)
+        {
+            this.Name = name;
+            this.Catagory = catagory;
+            this.Description = description;
+            this.Price = price;
+        }
+    }
+}
