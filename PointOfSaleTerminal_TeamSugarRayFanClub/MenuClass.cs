1f2c31ce5632b649b7aa0bfa9785598a1b003057
jbultsma <joshuabultsma@gmail.com>
2019-08-29 19:38:18 -0400

    git ignore

--- PointOfSaleTerminal_TeamSugarRayFanClub/MenuClass.cs
+++ PointOfSaleTerminal_TeamSugarRayFanClub/MenuClass.cs
@@ -9,7 +9,9 @@ namespace PointOfSaleTerminal_TeamSugarRayFanClub
         static void MainMenu()
         {
             bool isBuyAgain = false;
-
+            Console.WriteLine("Hello!, Welcome to the Sugar Ray Fan Club Merch Store!");
+            Console.WriteLine("Here you can buy all things Sugar Ray");
+            Console.WriteLine("It's pretty cool");
             do
             {
                 DisplayItems();
@@ -21,17 +23,19 @@ namespace PointOfSaleTerminal_TeamSugarRayFanClub
 
         static void DisplayItems()
         {
+            Database database = new Database();
+            Console.WriteLine("DisplayItems");
 
         }
 
         static void PurchaseItems()
         {
-
+            Console.WriteLine("PurchaseItems");
         }
 
         static void PrintReceipt()
         {
-
+            Console.WriteLine("PrintReceipt");
         }
        
     }
