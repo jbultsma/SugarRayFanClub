2c8b8761e064d4d1477ecec27617f16743ed4f23
Sam Thomson <samuelthomson62@gmail.com>
2019-08-28 21:31:49 -0400

    Created a Total.cs class that is a model for subtotal, sales tax and grand total

---
+++ PointOfSaleTerminal_TeamSugarRayFanClub/Total.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointOfSaleTerminal_TeamSugarRayFanClub
+{
+    class Total
+    {
+        public double subTotal { get; set; }
+        public double salesTax { get; }
+        public double grandTotal { get; set; }
+        public Total()
+        {
+            this.salesTax = .06;
+        }
+        public double GetTax(double s)
+        {
+            double grandTotal = s * salesTax;
+            return grandTotal;
+        }
+    }
+}
