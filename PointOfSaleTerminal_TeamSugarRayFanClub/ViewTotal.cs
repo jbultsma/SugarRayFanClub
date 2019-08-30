89568f43feb78113b940b8c6b0df219d6f9cf75c
Sam Thomson <samuelthomson62@gmail.com>
2019-08-29 20:54:53 -0400

    Built out ViewTotal.cs and GetList() method in Database.cs

--- PointOfSaleTerminal_TeamSugarRayFanClub/ViewTotal.cs
+++ PointOfSaleTerminal_TeamSugarRayFanClub/ViewTotal.cs
@@ -6,6 +6,11 @@ namespace PointOfSaleTerminal_TeamSugarRayFanClub
 {
     class ViewTotal
     {
-        Console.WriteLine("Subtotal: " + );
+        public void DisplayTotal(Total t)
+        {           
+            Console.WriteLine("Subtotal: " + t.subTotal);
+            Console.WriteLine("Tax: " + t.tax);
+            Console.WriteLine("Grand Total: " + t.grandTotal);
+        }
     }
 }
