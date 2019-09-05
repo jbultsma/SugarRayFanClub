72cb179afbd7cbac84f07d7acfcff60b5a617176
Sam Thomson <samuelthomson62@gmail.com>
2019-09-05 09:43:51 -0400

    Created a continue loop that is two while loops in the MenuClass

--- PointOfSaleTerminal_TeamSugarRayFanClub/MenuClass.cs
+++ PointOfSaleTerminal_TeamSugarRayFanClub/MenuClass.cs
@@ -13,42 +13,65 @@ namespace PoSMidterm
 
         public void Intro()
         {
-            bool isBuyAgain = false;
-            Console.WriteLine("Hello!, Welcome to the Sugar Ray Fan Club Merch Store!");
-            Console.WriteLine("Here you can buy all things Sugar Ray");
-            Console.WriteLine("It's pretty cool");
-            Console.WriteLine("Below you can find a list of things to pay to fill all of your Sugar Ray needs!");
-            Console.WriteLine();
-
-            do
+            bool runAgain = true, endLoop = true;
+            while (runAgain == true)
             {
-                c.DisplayMenu();
-                c.AddToCartDB(c.Products, c.ShoppingCart);
-                Console.Write("Would you like to buy another item? (y/n): ");
-                string input = Console.ReadLine().ToLower().Trim();
-                //Console.Clear();
+                bool isBuyAgain = false;
+                Console.WriteLine("Hello!, Welcome to the Sugar Ray Fan Club Merch Store!");
+                Console.WriteLine("Here you can buy all things Sugar Ray");
+                Console.WriteLine("It's pretty cool");
+                Console.WriteLine("Below you can find a list of things to pay to fill all of your Sugar Ray needs!");
+                Console.WriteLine();
 
-                if (input == "y")
+                do
                 {
-                    Console.WriteLine();
-                    isBuyAgain = true;
+                    c.DisplayMenu();
+                    c.AddToCartDB(c.Products, c.ShoppingCart);
+                    Console.Write("Would you like to buy another item? (y/n): ");
+                    string input = Console.ReadLine().ToLower().Trim();
+                     //Console.Clear();
+
+                    if (input == "y")
+                    {
+                        Console.WriteLine();
+                        isBuyAgain = true;
+                    }
+                    else
+                    {
+                        isBuyAgain = false;
+                    }
                 }
-                else
+                while (isBuyAgain == true);
+
+                c.DisplayShoppingCart(c.ShoppingCart);
+                    
+                Console.WriteLine();
+                t.SetTotal(c.ShoppingCart);
+                t.DisplayTotal();
+                Console.WriteLine();
+
+                r.PaymentMenu(t);
+
+                Console.WriteLine("Would you like to make another transaction? (y/n)");
+                while (endLoop == true)
                 {
-                    isBuyAgain = false;
+                    string answer2 = Console.ReadLine().ToLower().Trim();
+                    if (answer2 == "n")
+                    {
+                        runAgain = false;
+                        endLoop = false;
+                    }
+                    else if (answer2 =="y")
+                    {
+                        runAgain = true;
+                        endLoop = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter (y/n)");
+                    }
                 }
             }
-            while (isBuyAgain == true);
-
-            c.DisplayShoppingCart(c.ShoppingCart);
-
-            Console.WriteLine();
-            t.SetTotal(c.ShoppingCart);
-            t.DisplayTotal();
-            Console.WriteLine();
-
-            r.PaymentMenu(t);
-
         }
     }
 }
